2c9a91edea143d423818f5c42b871f6337880ccf
Pedro H. P. Fontana <fontana@Pedros-MacBook-Air.fritz.box>
2024-08-27 22:52:45 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='autoencoders',
-    version='0.1',
-    packages=find_packages(),
+    version='0.0.1',
+    author='Fontana, P.',
+    description='Autoencoders models',
+    url='https://github.com/phpfontana/autoencoders/tree/main',
+    package_dir={'': 'autoencoders'},
+    packages=find_packages(where='autoencoders'),
     install_requires=[
         'torch==2.4.0',
         'torchaudio==2.4.0',
